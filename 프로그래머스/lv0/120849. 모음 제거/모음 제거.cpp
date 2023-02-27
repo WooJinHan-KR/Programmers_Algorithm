b1b9e7c4c267dbdcb90506aa9ff6d01b47a57998
gks970113-woo <81940197+gks970113-woo@users.noreply.github.com>
2023-02-27 18:41:57 +0900

    [level 0] Title: 모음 제거, Time: 0.01 ms, Memory: 4.22 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120849. 모음 제거/모음 제거.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+string solution(string my_string) {
+    string answer = "";
+    for(int i=0; i<my_string.size(); i++){
+        if(my_string[i]=='a'|| my_string[i]=='e'|| my_string[i]=='i'|| my_string[i]=='o'|| my_string[i]=='u'){
+            my_string.erase(my_string.begin()+i);
+            i--;
+        }
+    }
+    answer = my_string;
+    return answer;
+}
