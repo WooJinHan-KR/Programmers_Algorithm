678b5da07e129acbe6c0a968b6faa817ccd5f09f
gks970113-woo <81940197+gks970113-woo@users.noreply.github.com>
2023-02-27 19:51:38 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 0.05 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+vector<string> solution(vector<string> strings, int n) {
+    vector<string> answer;
+    vector<pair<char, string>> v;
+    for(int i=0; i<strings.size(); i++){
+        v.push_back(pair<char, string>(strings[i][n], strings[i]));
+    }
+    sort(v.begin(), v.end());
+    for(int i=0; i<v.size(); i++)
+        answer.push_back(v[i].second);
+    return answer;
+}
