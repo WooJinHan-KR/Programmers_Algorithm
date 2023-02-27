a23377acb00be38209a16f1abae5a53de61a94bc
gks970113-woo <81940197+gks970113-woo@users.noreply.github.com>
2023-02-27 18:31:40 +0900

    [level 0] Title: A로 B 만들기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120886. A로 B 만들기/A로 B 만들기.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(string before, string after) {
+    int answer = 0;
+    sort(before.begin(), before.end());
+    sort(after.begin(), after.end());
+    
+    if(after == before)
+        answer = 1;
+    else
+        answer = 0;
+    return answer;
+}
