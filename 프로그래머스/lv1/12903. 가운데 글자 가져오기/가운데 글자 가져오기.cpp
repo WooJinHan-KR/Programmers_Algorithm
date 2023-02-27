3b04b5291f254622730a8955bfa46e371a993b9e
gks970113-woo <81940197+gks970113-woo@users.noreply.github.com>
2023-02-27 19:52:43 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string s) {
+    string answer = "";
+    if(s.size()%2 == 0)
+        answer = answer + s[s.size()/2-1] + s[s.size()/2];
+    else
+        answer += s[s.size()/2];
+    return answer;
+}
