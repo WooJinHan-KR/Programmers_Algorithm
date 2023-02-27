b8e12bb59683481e2cfc540a3459ff6084b82139
gks970113-woo <81940197+gks970113-woo@users.noreply.github.com>
2023-02-27 19:10:29 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.01 ms, Memory: 4.17 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120806. 두 수의 나눗셈/두 수의 나눗셈.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int num1, int num2) {
+    int answer = 0;
+    int num3 = num1 * 1000 / num2;
+    answer = num3;
+    return answer;
+}
