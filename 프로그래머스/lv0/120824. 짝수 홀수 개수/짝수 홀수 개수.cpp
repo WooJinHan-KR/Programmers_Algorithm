d93eb9ac7d65e898d7712fd89d31e7873f17e98a
gks970113-woo <81940197+gks970113-woo@users.noreply.github.com>
2023-02-27 18:59:01 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> num_list) {
+    vector<int> answer(0);
+    answer.push_back(0);
+    answer.push_back(0);
+    for(int i=0; i<num_list.size(); i++){
+        if(num_list[i]%2==0)
+            answer[0] = answer[0]+1;
+        else
+            answer[1] = answer[1]+1;
+    }
+    return answer;
+}
