d421a16eb1da5cec4a3a165b89a38f75a2e253f9
onerainJ <81940197+gks970113-woo@users.noreply.github.com>
2023-03-29 09:48:23 +0900

    [Bronze V] Title: A+B - 7, Time: 4 ms, Memory: 2016 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main(){
+	
+    int a, b, n;
+  	
+	cin >> n;
+	
+	for(int i=1; i<=n; i++)
+	{
+		cin >> a >> b;
+		cout << "Case #" << i << ": "<< a+b << "\n";
+	}
+		
+
+    return 0;
+}
