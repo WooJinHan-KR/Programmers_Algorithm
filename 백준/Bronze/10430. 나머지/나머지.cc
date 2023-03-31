463b322f0788171a9bbd09a29e8ad9258e26417d
onerainJ <81940197+gks970113-woo@users.noreply.github.com>
2023-03-31 23:06:09 +0900

    [Bronze V] Title: 나머지, Time: 0 ms, Memory: 2016 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main(){
+    int A, B, C;
+  	
+	cin >> A >> B >> C;
+
+    cout <<  (A+B)%C << endl;
+    cout << ((A%C) + (B%C))%C << endl;
+    cout << (A*B)%C << endl;
+    cout << ((A%C) * (B%C))%C << endl;
+    
+    return 0;
+}
