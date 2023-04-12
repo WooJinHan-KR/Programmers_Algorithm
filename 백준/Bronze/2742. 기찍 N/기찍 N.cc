d6dfa6aa4ef50f33b66c84e6e8ca47976a2462d5
onerainJ <81940197+gks970113-woo@users.noreply.github.com>
2023-04-13 02:53:55 +0900

    [Bronze IV] Title: 기찍 N, Time: 12 ms, Memory: 2016 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main(){
+	
+    int n;
+  	
+	cin >> n;
+	
+	for(int i=n; i>=1; i--)
+	{
+		cout << i << "\n";
+	}
+		
+
+    return 0;
+}
