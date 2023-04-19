ec2ec9dfdb8aaef1e43c01aa6de62c0891e48e77
onerainJ <81940197+gks970113-woo@users.noreply.github.com>
2023-04-19 20:56:20 +0900

    [Bronze V] Title: ??!, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    
+    string a;
+    
+    cin >> a;
+    
+    a += "??!";
+        
+    cout << a;
+    
+    return 0;
+}
